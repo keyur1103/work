d99e22b980fb55c3ebbf6132845b1241901521bb
keyur1103 <40786801+keyur1103@users.noreply.github.com>
2018-09-05 19:08:27 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,21 @@
 
- var cloudinary = require('cloudinary');
+ const cloudinary = require('cloudinary');
+
 
 cloudinary.config({ cloud_name: 'keyur1103', api_key: 423612485853384, api_secret: 'e2ZzOCPaheH4BvC6nPazouw-FkU'  });
 
-cloudinary.v2.api.resources({ type: 'upload', prefix: 'sss/' }, function(error, result){console.log(result)});
+var new_arrary = [];
+ cloudinary.v2.api.resources({ type: 'upload', prefix: 'sss/' }, function(error, result){result;
+    var array = [result.resources];
+    console.log(array[0].length)
+ array[0].forEach(function(element){
+        var temp = {};
+        var r = [element];
+        var key = r[0].public_id.split('/')[1];
+        var value = r[0].url;
+        temp[key] = value;
+        new_arrary.push(temp);
+    });
+    console.log('@@@@@@@@@@@@@@@@',new_arrary);
+});
+
