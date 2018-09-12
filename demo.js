34284afba1a0a7380f3ff20c1ced2c8c064a47f9
keyur1103 <40786801+keyur1103@users.noreply.github.com>
2018-09-12 18:48:51 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,63 +1,89 @@
+const cloudinary = require('cloudinary');
+let fs = require('fs');
+// const Json2csvParser = require('json2csv').Parser;
+const fields = ['name', 'url'];
+const Json2csvParser = require('json2csv').Parser;
 
- const cloudinary = require('cloudinary');
- const async = require('asyncawait/async');
- const await = require('asyncawait/await');
- var csv_export=require('csv-export');
- var fs = require('fs');
-
-cloudinary.config({ cloud_name: 'keyur1103', api_key: 423612485853384, api_secret: 'e2ZzOCPaheH4BvC6nPazouw-FkU'  });
+cloudinary.config({
+    cloud_name: 'keyur1103',
+    api_key: 423612485853384,
+    api_secret: 'e2ZzOCPaheH4BvC6nPazouw-FkU'
+});
 
 begin();
 async function begin() {
-    // let optimizeImage = await processImage()
-    // console.log('========', optimizeImage) 
-    processImage()
-} 
-
-function generateCSV () {
-    var new_arrary = [];
-    cloudinary.v2.api.resources({ type: 'upload', prefix: 'optimzeimage/' }, function(error, result){result;
-        var array = [result.resources];
-        var myObj = []
-        array[0].forEach(function(element){
-            var temp = {};
-            var r = [element];
-            var key = r[0].public_id.split('/')[1];
-            var value = r[0].url;
-            console.log("key",key);
-            console.log("value",value);
-            myObj.push({ 'public_id': key, 'url': value });
-
-        });
-        csv_export.export(myObj,function(buffer){
-
-        fs.writeFileSync('/home/software/Desktop/mjmldemo/data.zip',buffer);  
-        });
-    });
+   await processImage()
+    // processImage();
+    await generateCSV();
+   
 }
 
-function processImage () {
-    return new Promise((resolve, reject) => {
-        cloudinary.v2.api.resources({ type: 'upload', prefix: 'sss/' }, function(error, result){
+async function processImage() {
+    console.log('processimage')
+    // return new Promise((resolve, reject) => {
+        await cloudinary.v2.api.resources({
+            type: 'upload',
+            prefix: 'sss/'
+        }, async function (error, result) {
             // result;
-            var array = [result.resources];
-            // console.log(array[0].length)
-            array[0].forEach(function(element){
-                var temp = {};
-                var r = [element];
-                var key = r[0].public_id.split('/')[1];
-                var value = r[0].url;
-        
-                cloudinary.v2.uploader.upload(value,
-                { folder: "optimzeimage/",quality: "auto:good"},
-                function(error, result) {console.log(result, error); });
-            });
-        });
-        generateCSV();
-        resolve('image process done..!')
-    })
-    // Promise.resolve(_promise).then((res) => {
-    //     return res
-    // })
-}
+            let array = [result.resources];
+            console.log(array[0].length)
+            array[0].forEach(async function (element) {
+                let temp = {};
+                let r = [element];
+                let key = r[0].public_id.split('/')[1];
+                let value = r[0].url;
 
+                await cloudinary.v2.uploader.upload(value, {
+                        folder: "optimzeimage/",
+                        quality: "auto:good"
+                    },
+                   function (error, result) {
+                       
+                       
+                    });
+
+            });
+          
+        });
+
+        // resolve('image process done..!')
+         
+        // generateCSV(); 
+        
+        // generateCSV();
+    // })
+    // await generateCSV();
+    console.log('endof processimage')
+}
+async function generateCSV() {
+    console.log('generatecsv')
+    let new_arrary = [];
+
+    await cloudinary.v2.api.resources({
+        type: 'upload',
+        prefix: 'optimzeimage/'
+    }, function (error, result) {
+        // result;
+        
+        let array = [result.resources];
+        array[0].forEach(function (element) {
+            let temp = {}
+            let r = [element];
+            let name = r[0].public_id.split('/')[1];
+            let url = r[0].url;
+             temp ={
+                "name": name,
+                "url": url,
+            } ;
+            new_arrary.push(temp);        
+           
+    });
+    var json2csvParser = new Json2csvParser({ fields });
+    var csv = json2csvParser.parse(new_arrary);
+    fs.writeFileSync('./clo.csv', csv)
+   
+}
+   
+     
+ )}
